54d57227e08d2e3273fc7cdf855c247f28dc3489
Jino Jossy <jinojossy93@gmail.com>
2020-03-25 09:14:02 +0530

    update stream config

--- big_data/src/configs/stream_source_config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'OtfNcgAJctM25bbOguIgyj8LY'
-consumer_secret = 'eM6DlYARUWv98CoG03eXfEVBAj3LnLPzJgi6rx1bcpRtBb4Eim'
-access_token = '150212272-tE4ZbF8gXYBoPI9rfyxpc1o3QVY2AdFQTHgXHYol'
-access_secret = '9ZoPcDCN7MwDY1bu926S9Yr8WnyEiArDKZD7jCUdZCjht'
