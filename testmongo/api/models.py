81c4047de9e0694c9d5ae684cb29a59c6ca2e9fc
Jino Jossy <jinojossy93@gmail.com>
2020-03-25 21:30:23 +0530

    remove testmongo project

--- testmongo/api/models.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import DynamicDocument, StringField, DateTimeField
-import datetime
-
-
-class TwitterData(DynamicDocument):
-    text = StringField(required=True, max_length=200)
-    created_at = DateTimeField(default=datetime.datetime.now())
-    meta = {'allow_inheritance': True}
--- testmongo/api/utils/constants.py
+++
@@ -1,6 +0,0 @@
-# Messages
-SUCCESS = 'success'
-FAIL = 'fail'
-UNAUTHORIZED = 'unauthorized'
-INVALID_PARAMETERS = 'invalid parameters'
-
