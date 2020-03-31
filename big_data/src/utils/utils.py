e2a2cfdbc560e7468235a83111971ae3d98068ae
chandan-urs <p.chandan1995@gmail.com>
2020-03-31 12:41:54 +0530

    Hashtags and Categories added

---
+++ big_data/src/utils/utils.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from utils import constants
+
+
+client = MongoClient('localhost', 27017)
+db = client[constants.DB_NAME]
+
+
+def getMandatoryHashtags():
+    hashtags = db.corona_tweet_analysis_hashtag
+    hashtags_list = [hashtag["hashtag"] for hashtag in hashtags.find({"approved": True}, {"_id": 0, "hashtag": 1})]
+    return hashtags_list
+
+
+def getCategories():
+    categories = db.corona_tweet_analysis_categorysql
+    categories_list = [category for category in categories.find({"approved": True},
+                                                                {'_id': 0, 'category': 1, 'keywords': 1})]
+    return categories_list
