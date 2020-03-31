c6900385bafa9daec8473b0dae47badd4ae86e90
chandan-urs <p.chandan1995@gmail.com>
2020-03-31 15:31:30 +0530

    Updated constants

--- big_data/src/utils/constants.py
+++ big_data/src/utils/constants.py
@@ -8,16 +8,3 @@ PASSWORD = "bigdatapassword"
 
 
 COUNTRIES = [country.name.lower() for country in list(countries)] + ['usa', 'worldwide']
-CATEGORIES = ['INFECTED', 'RECOVERED', 'DEATH', 'POSITIVE', 'NEGATIVE', 'TRAVEL_HISTORY']
-
-INFECTED_KEYWORDS = ['infected', 'infect', 'tested positive', 'confirmed', 'positive', 'new case']
-RECOVERED_KEYWORDS = ['recover', 'recovered', 'tested negative', 'negative']
-DEATH_KEYWORDS = ['death toll', 'died', 'deaths', 'death']
-TRAVEL_HISTORY_KEYWORDS = ['travel history']
-SUPPLY_KEYWORDS = ['demand', 'need']
-VACCINE_KEYWORDS = ['vaccine']
-CURE_KEYWORDS = ['cure']
-
-KEYWORDS = INFECTED_KEYWORDS + RECOVERED_KEYWORDS + DEATH_KEYWORDS + TRAVEL_HISTORY_KEYWORDS + VACCINE_KEYWORDS + CURE_KEYWORDS
-
-MANDATORY_HASHTAGS = ['COVID19', 'coronavirus', 'Corona', 'CoronaVirusUpdate','IndiaFightsCorona','CoronavirusLockdown','Covid19India','StayAtHomeOrder']
